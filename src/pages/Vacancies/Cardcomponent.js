06f6a8589ad3e74f8df515b1dec8c18a0937d15a
LuMeng9 <LuMeng9@outlook.com>
2020-08-12 03:28:42 +0800

    by menglu

--- src/pages/Vacancies/Cardcomponent.js
+++ src/pages/Vacancies/Cardcomponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -10,43 +10,60 @@ import "./Cardcomponent.scss";
 import { ReactComponent as StarIconOne } from "./star-1.svg";
 import { ReactComponent as StarIconTwo } from "./star-2.svg";
 import { ReactComponent as StarIconThree } from "./star-3.svg";
+import { render } from "@testing-library/react";
 
-export default function OutlinedCard() {
-  return (
-    <Card className="root" variant="outlined">
-      <CardContent>
-        <Typography className="title" color="textSecondary">
-          Created 12.04.2020
-        </Typography>
-        <Typography variant="h4" component="h1">
-          CAD and Meshing
-          <br />
-          Software Developer
-          <br />
-          (m/f/d)
-        </Typography>
-        <Typography color="textSecondary" style={{ marginTop: "20px" }}>
-          <VisibilityIcon color="primary" style={{ verticalAlign: "middle" }} />
-          &nbsp;&nbsp;236 views
-        </Typography>
-        <Typography color="textSecondary" style={{ marginTop: "13px" }}>
-          <DescriptionIcon color="primary" style={{ verticalAlign: "sub" }} />
-          &nbsp;&nbsp;16 Applications received
-        </Typography>
-        <Typography
-          style={{ color: "limegreen", marginTop: "9.3px", marginLeft: "3px" }}
-        >
-          <StarIconOne style={{ paddingBottom: "12px" }} />
-          <StarIconThree style={{ marginLeft: "-4px", marginRight: "-1px" }} />
-          <StarIconTwo style={{ paddingBottom: "9px" }} />
-          &nbsp;&nbsp;Anton DIMITROVA has been hired!
-        </Typography>
-      </CardContent>
-      <CardActions style={{ float: "right" }}>
-        <Button color="primary" style={{ fontWeight: "bold" }}>
-          More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+class Cardcomponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Card className="root" variant="outlined">
+        <CardContent>
+          <Typography className="title" color="textSecondary">
+            Created {this.props.createdDate}
+          </Typography>
+          <Typography variant="h4" component="h1">
+            CAD and Meshing
+            <br />
+            Software Developer
+            <br />
+            (m/f/d)
+          </Typography>
+          <Typography color="textSecondary" style={{ marginTop: "20px" }}>
+            <VisibilityIcon
+              color="primary"
+              style={{ verticalAlign: "middle" }}
+            />
+            &nbsp;&nbsp;{this.props.viewsNum} views
+          </Typography>
+          <Typography color="textSecondary" style={{ marginTop: "13px" }}>
+            <DescriptionIcon color="primary" style={{ verticalAlign: "sub" }} />
+            &nbsp;&nbsp;{this.props.appReceivedNum} Applications received
+          </Typography>
+          <Typography
+            style={{
+              color: "limegreen",
+              marginTop: "9.3px",
+              marginLeft: "3px"
+            }}
+          >
+            <StarIconOne style={{ paddingBottom: "12px" }} />
+            <StarIconThree
+              style={{ marginLeft: "-4px", marginRight: "-1px" }}
+            />
+            <StarIconTwo style={{ paddingBottom: "9px" }} />
+            &nbsp;&nbsp;Anton DIMITROVA has been hired!
+          </Typography>
+        </CardContent>
+        <CardActions style={{ float: "right" }}>
+          <Button color="primary" style={{ fontWeight: "bold" }}>
+            More
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
+
+export default Cardcomponent;
--- src/pages/Vacancies/index.js
+++ src/pages/Vacancies/index.js
@@ -1,3 +1,31 @@
+import React, { Component } from "react";
 import Cardcomponent from "./Cardcomponent";
 
-export default Cardcomponent;
+class Vacancies extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div style={{ display: "inline-flex" }}>
+        <Cardcomponent
+          createdDate="12.04.2020"
+          viewsNum="236"
+          appReceivedNum="16"
+        />
+        <Cardcomponent
+          createdDate="12.04.2020"
+          viewsNum="198"
+          appReceivedNum="23"
+        />
+        <Cardcomponent
+          createdDate="12.04.2020"
+          viewsNum="349"
+          appReceivedNum="31"
+        />
+      </div>
+    );
+  }
+}
+
+export default Vacancies;
